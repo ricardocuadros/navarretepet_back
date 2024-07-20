4483a6219eff03d227ab1830ec52a10b77d08f51
RicardoCuadros <ricardo22042003@gmail.com>
2024-07-20 03:10:22 -0500

    primer cambio

---
+++ src/main/java/com/zosh/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.zosh.config;
+
+public class JwtConstant {
+	public static final String SECRET_KEY = "mkmiondnawijdawbdhbawhdbahwbdhawbdgawvdgajwgd";
+	public static final String JWT_HEADER = "Authorization";
+}
